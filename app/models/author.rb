f35d1d7809ce390b5d1fe2dde19d66798caefa98
github username <github email address>
2019-04-28 13:16:50 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ActiveRecord::Base
+  #all authors have a name
+  validates :name, presence: true
+  #no two authors have the same name
+  validates :name, uniqueness: { case_sensitive: false }
+  #author phone numbers are exactly ten digits
+  validates :phone_number, length: { is: 10}
 end
